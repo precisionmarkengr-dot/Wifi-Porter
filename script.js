9808d3ef0cf7a3719b8cf441bab2525178e9d3cb
precisionmarkengr-dot <precisionmarkengr@gmail.com>
2025-11-25 11:35:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,27 @@
-document.getElementById('downloadProfile').onclick = function() {
+function generate() {
   var ssid = document.getElementById('ssid').value;
   var password = document.getElementById('password').value;
   var security = document.getElementById('security').value;
-  var profile =
-`<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>PayloadContent</key>
-    <array>
-      <dict>
-        <key>EncryptionType</key>
-        <string>${security === 'nopass' ? '' : security}</string>
-        <key>Password</key>
-        <string>${security === 'nopass' ? '' : password}</string>
-        <key>SSID_STR</key>
-        <string>${ssid}</string>
-        <key>PayloadType</key>
-        <string>com.apple.wifi.managed</string>
-        <key>PayloadVersion</key>
-        <integer>1</integer>
-        <key>PayloadIdentifier</key>
-        <string>wifi.porter.setup</string>
-        <key>PayloadDisplayName</key>
-        <string>Wi-Fi Porter Setup</string>
-        <key>AutoJoin</key>
-        <true/>
-      </dict>
-    </array>
-    <key>PayloadType</key>
-    <string>Configuration</string>
-    <key>PayloadVersion</key>
-    <integer>1</integer>
-    <key>PayloadIdentifier</key>
-    <string>wifi.porter.setup</string>
-    <key>PayloadDisplayName</key>
-    <string>Wi-Fi Porter Setup</string>
-    <key>PayloadDescription</key>
-    <string>Configures Wi-Fi settings</string>
-    <key>PayloadRemovalDisallowed</key>
-    <false/>
-    <key>PayloadUUID</key>
-    <string>12345678-1234-5678-1234-${Date.now()}</string>
-  </dict>
-</plist>`;
-  var blob = new Blob([profile], {type: 'application/x-apple-aspen-config'});
-  var link = document.createElement('a');
-  link.href = window.URL.createObjectURL(blob);
-  link.download = 'wifi.mobileconfig';
-  link.click();
-};
+
+  // Create custom setup link for NFC tag
+  var setupUrl = `https://setup.precisionmarkengraving.com/?ssid=${encodeURIComponent(ssid)}&password=${encodeURIComponent(password)}&security=${encodeURIComponent(security)}`;
+  document.getElementById('wifiString').value = setupUrl;
+
+  // Make QR code
+  var qrString;
+  if (security === 'nopass') {
+    qrString = `WIFI:T:;S:${ssid};;`;
+  } else {
+    qrString = `WIFI:T:${security};S:${ssid};P:${password};;`;
+  }
+
+  document.getElementById('qrcode').innerHTML = "";
+  QRCode.toCanvas(qrString, { width: 180 }, function (error, canvas) {
+    if (!error) {
+      document.getElementById('qrcode').appendChild(canvas);
+    }
+  });
+}
+
+// This makes sure the function is available for your HTML button
+window.generate = generate;
