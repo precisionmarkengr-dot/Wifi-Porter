4da1fa03e7470968046b4204ef563e298b857f65
precisionmarkengr-dot <precisionmarkengr@gmail.com>
2025-11-25 12:56:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -58,9 +58,12 @@ window.generate = generate;
 
 // RFC-compliant UUID for Apple mobileconfig PayloadUUID
 function generateUUID() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
+  // More reliable UUID v4 generation
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0;
+    var v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
 
 // iPhone Wi-Fi profile download
@@ -119,4 +122,3 @@ document.getElementById('downloadProfile').onclick = function() {
   link.click();
   document.body.removeChild(link);
 };
-
